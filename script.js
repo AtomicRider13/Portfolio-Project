b28040370dfffd873f6643abcfddaaf5aaf86a2f
AtomicRider13 <143289316+AtomicRider13@users.noreply.github.com>
2024-05-05 20:20:10 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const tableRowElements = document.getElementsByTagName("tr");
+const dateElement = document.getElementById("date");
+const timeElement = document.getElementById("time");
+const clickAudioElement = document.getElementById("click-sound");
+const popUpDisplay = document.getElementById("project-popup");
+const popUpCloseBtn = document.getElementById("project-popup-closeBtn");
+
+Array.from(tableRowElements).forEach((element) => {
+    element.addEventListener("click", () => {
+        
+        clickAudioElement.play();
+        const projectName = element.dataset.name;
+        console.log(projectName);
+        const projectDescription = element.dataset.description;
+        if (projectName && projectDescription) {
+            popUpDisplay.classList.remove("hidden");
+            const popUpTitle = document.getElementById("project-popup-title");
+            popUpTitle.innerText = projectName;
+            const popUpDescription = document.getElementById("project-popup-description");
+            popUpDescription.innerText = projectDescription;
+        } else {
+            alert("Error: No project information found");
+        }
+    });
+});
+
+popUpCloseBtn.addEventListener("click", () => {
+    clickAudioElement.play();
+    popUpDisplay.classList.add("hidden");
+})
+
+
+
+
+
+
+
